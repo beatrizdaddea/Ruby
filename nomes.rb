b0b0d7d5001d2a3ce11eefc0a8000abd9c2838c1
Beatriz D'addead <beatrizchith@gmail.com>
2022-06-16 14:21:55 -0300

    update

---
+++ nomes.rb
@@ -0,0 +1,14 @@
+class Nome ()
+     def initialize(nome = "Mundo")
+        @nome = nome
+      end
+      def diz_ola
+        puts "Olá #{@nome}!"
+      end
+      def diz_adeus
+        puts "Adeus #{@nome}, volte sempre."
+      end
+end 
+
+nome = Nome.new("João")
+nome.diz_ola()
