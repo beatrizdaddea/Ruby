ebfa8eb0abcc413d24fac7542bc1c7266d740fb0
Beatriz D'addead <beatrizchith@gmail.com>
2022-06-19 19:48:37 -0300

    study

---
+++ RubyIniciantes/Condicionais em Ruby/if.rb
@@ -0,0 +1,11 @@
+# puts false && true --> false
+# puts false || true --> true
+# puts !false && false --> false
+
+x = 10 
+
+if x == 11 
+    puts "O valor de X é 10"
+else
+    puts " X é um valor diferente de 10"
+end 
