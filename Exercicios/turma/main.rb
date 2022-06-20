a4e1356641283e48b8796352884ea54a6ff9b4fb
Beatriz D'addead <beatrizchith@gmail.com>
2022-06-20 16:51:46 -0300

    study

---
+++ Exercicios/turma/main.rb
@@ -0,0 +1,12 @@
+require_relative "aluno"
+require_relative "professor"
+require_relative "turma"
+
+a1 = Aluno.new("beatriz", 21,123)
+a2 = Aluno.new("rafaela", 20,124)
+
+p1 = Professor.new("pedro", 50,"Matematica")
+
+t1 = Turma.new([a1,a2], p1)
+t1.alunos.push(a1,a2)
+t1.dados_turma
---
+++ Exercicios/turma/turma.rb
@@ -0,0 +1,21 @@
+class Turma
+    attr_reader :alunos, :professor 
+        
+    def initialize(aluno, professor)
+        @alunos = Array.new
+        @professor = professor
+    end 
+
+
+    def dados_turma 
+        puts "Disciplina: #{professor.disciplina} Professor/a: #{professor.nome} "
+        listar_alunos
+    end 
+
+    private 
+    def listar_alunos
+        @alunos.each do |aluno|
+            puts "Nome: #{aluno.nome} Matricula:#{aluno.matricula} "
+        end 
+    end
+end
